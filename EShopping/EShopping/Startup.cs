9e90de09e1beeda14b8adad07637378de8ffe6fa
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-07-19 02:20:12 +0700

    Cập Nhật Mã Nguồn và Cây Thư Mục

---
+++ EShopping/EShopping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EShopping.Startup))]
+namespace EShopping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

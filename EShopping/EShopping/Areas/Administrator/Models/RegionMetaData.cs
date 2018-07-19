d18be9ec6f8e7f469d3d7f6fe5ac9da886ed3ad3
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-07-19 23:41:42 +0700

    Region

---
+++ EShopping/EShopping/Areas/Administrator/Models/RegionMetaData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EShopping.Models;
+using System.ComponentModel.DataAnnotations;
+namespace EShopping.Areas.Administrator.Models
+{
+      [MetadataTypeAttribute(typeof(Region.RegionMetaData))]
+    public partial class Region
+    {
+          internal sealed class RegionMetaData
+          {
+              public int RegionId { get; set; }
+               [Display(Name = "Region")]
+              public string RegionName { get; set; }
+               [Display(Name = "Description")]
+              public string RegionDescription { get; set; }
+          }
+    }
+}

15c381b939436e26de87537d2fcb5deb19e5d82f
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-07-19 23:32:38 +0700

    Bổ sung MetaData

---
+++ EShopping/EShopping/Areas/Administrator/Models/CategoryMetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EShopping.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EShopping.Models
+{
+    [MetadataTypeAttribute(typeof(Category.CategoryMetaData))]
+    public partial class  Category
+    {
+        internal sealed class CategoryMetaData
+        {
+            public int CategoryId { get; set; }
+            [Display(Name="Category")]
+            public string CategoryName { get; set; }
+            [Display(Name = "Details")]
+            public string CategoryDetails { get; set; }
+        }
+    }
+}

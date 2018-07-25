45448ee469aa573276adcf6db7db65682d09ba03
TTPi <phongb1505902@student.ctu.edu.vn>
2018-07-25 13:20:03 +0700

    update News Pagin

--- EShopping/EShopping/Controllers/NewController.cs
+++ EShopping/EShopping/Controllers/NewController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
 
 namespace EShopping.Controllers
 {
@@ -11,10 +13,20 @@ namespace EShopping.Controllers
     {
         ElectricStoreEntities db = new ElectricStoreEntities();
         // GET: New
-        public ActionResult News(string sortOrder, string searchString)
+        public ActionResult News(string sortOrder, string searchString, int? page, string currentFilter)
         {
+            ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
             var lstNew = from s in db.News
                          select s;
             if (!String.IsNullOrEmpty(searchString))
@@ -22,8 +34,11 @@ namespace EShopping.Controllers
                 lstNew = lstNew.Where(s => s.NewTitles.Contains(searchString)
                                        || s.NewsBy.Contains(searchString));
             }
+            
                     lstNew = lstNew.OrderBy(s => s.NewUpdate);
-            return View(lstNew);
+                    int pageSize = 3;
+                    int pageNumber = (page ?? 1);
+                    return View(lstNew.ToPagedList(pageNumber, pageSize));
         }
     }
 }
